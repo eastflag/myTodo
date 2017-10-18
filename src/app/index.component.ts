1364bcb4b23436fb82c0d2e60def3717bc0108f1
eastflag <eastflag@gmail.com>
2017-10-19 00:59:51 +0900

    로그인 로직 변경

--- src/app/index.component.ts
+++ src/app/index.component.ts
@@ -1,4 +1,8 @@
 import {Component} from "@angular/core";
+import {AuthGuardService} from "./auth/auth-guard.service";
+import {AngularFireAuth} from "angularfire2/auth";
+import {Observable} from "rxjs/Observable";
+import * as firebase from "firebase";
 
 @Component({
   templateUrl: './index.component.html',
@@ -6,4 +10,15 @@ import {Component} from "@angular/core";
 })
 export class IndexComponent {
 
+  constructor(private authService: AuthGuardService) {
+
+  }
+
+  isAuthenticated() {
+    return this.authService.isAuthenticated();
+  }
+
+  logout() {
+    this.authService.logOut();
+  }
 }
